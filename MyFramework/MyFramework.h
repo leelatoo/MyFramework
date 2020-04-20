66f4e880c93330d035714fb3a7feed274312d3a2
Leela <leeladevi.balasundaram@ey.com>
2020-04-20 12:48:17 -0400

    Initial Commit

---
+++ MyFramework/MyFramework.h
@@ -0,0 +1,19 @@
+//
+//  MyFramework.h
+//  MyFramework
+//
+//  Created by Leeladevi Balasundaram on 4/20/20.
+//  Copyright © 2020 Leeladevi Balasundaram. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MyFramework.
+FOUNDATION_EXPORT double MyFrameworkVersionNumber;
+
+//! Project version string for MyFramework.
+FOUNDATION_EXPORT const unsigned char MyFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyFramework/PublicHeader.h>
+
+
